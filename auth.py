db5238f34479b5fadfc887b26b96c594a2bce64d
Young-Ho Cha <ganadist@gmail.com>
2012-12-01 21:49:03 +0900

    initial commit
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

---
+++ auth.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+USERNAME = 'username'
+PASSWORD = 'password'
+"""
+    <option selected="selected" value="">카드선택</option>
+    <option value="BCC">BC카드</option>
+    <option value="SIN">신세계카드</option>
+    <option value="NLC">NH카드</option>
+    <option value="HNB">하나SK카드</option>
+    <option value="KJB">광주VISA카드</option>
+    <option value="SAN">산은캐피탈</option>
+    <option value="NFF">수협카드</option>
+    <option value="CNB">국민카드</option>
+    <option value="TMP">삼성카드(올앳포함)</option>
+    <option value="SHB">신한카드(구,LG포함)</option>
+    <option value="DIN">현대카드</option>
+    <option value="KEB">외환카드</option>
+    <option value="PHB">우리(평화)카드</option>
+    <option value="AMX">롯데/아멕스카드</option>
+    <option value="CIT">씨티카드(구,한미)</option>
+"""
+CARDTYPE = 'BCC'
+CARDNUM = '1234 5678 9012 3456'
+CARDVALID = '01/13' # MM/YY
+CARDPASS  = '1234'
+SSN = '123456-7890123'
